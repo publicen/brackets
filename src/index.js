3162a3fbe8564dde089784a3b7f5e9af34359646
Alexander <70852922+publicen@users.noreply.github.com>
2020-10-21 01:10:16 +0300

    Docs: add a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0) return false
+  let bracketsArray = bracketsConfig.map(a=>a.join(""))
+  for (let i = 0; i < bracketsArray.length; i++){
+    if (str.includes(bracketsArray[i])){
+      str= str.replace(bracketsArray[i], "")
+      i -= bracketsArray.length
+    }
+   }  
+  return str.length === 0 ? true : false  
 }
+  
